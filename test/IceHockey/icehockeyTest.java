6f3e722a3af69e415f08587086fa46af9b6a0cb3
MohamedRadwan2000 <60818439+MohamedRadwan2000@users.noreply.github.com>
2020-03-06 03:15:03 +0200

    Update icehockeyTest.java

--- test/IceHockey/icehockeyTest.java
+++ test/IceHockey/icehockeyTest.java
@@ -1,7 +1,7 @@
 package IceHockey;
 
 
-
+//mohamed mohamed mostafa radwan
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -13,7 +13,13 @@ class icehockeyTest {
     @Test
     void findPlayers() {
         icehockey x=new icehockey();
-        String[] photo=new String[]{"33JUBU33", "3U3O4433", "O33P44NB", "PO3NSDP3", "VNDSD333", "OINFD33X"};
+        String[] photo=new String[]{
+                "33JUBU33",
+                "3U3O4433",
+                "O33P44NB",
+                "PO3NSDP3",
+                "VNDSD333",
+                "OINFD33X"};
         Point[]test=new Point[3];
         test[0]=new Point();
         test[1]=new Point();
@@ -27,7 +33,17 @@ class icehockeyTest {
     @Test
     void test2() {
         icehockey x=new icehockey();
-        String[] photo=new String[]{"44444H44S4", "K444K4L444", "4LJ44T44XH", "444O4VIF44", "44C4D4U444", "4V4Y4KB4M4", "G4W4HP4O4W", "4444ZDQ4S4", "4BR4Y4A444", "4G4V4T4444"};
+        String[] photo=new String[]{
+                "44444H44S4",
+                "K444K4L444",
+                "4LJ44T44XH",
+                "444O4VIF44",
+                "44C4D4U444",
+                "4V4Y4KB4M4",
+                "G4W4HP4O4W",
+                "4444ZDQ4S4",
+                "4BR4Y4A444",
+                "4G4V4T4444"};
         Point[]test=new Point[6];
         test[0]=new Point(3,8);
         test[1]=new Point(4,16);
@@ -41,7 +57,22 @@ class icehockeyTest {
     @Test
     void test3() {
         icehockey x=new icehockey();
-        String[] photo=new String[]{"8D88888J8L8E888", "88NKMG8N8E8JI88", "888NS8EU88HN8EO", "LUQ888A8TH8OIH8", "888QJ88R8SG88TY", "88ZQV88B88OUZ8O", "FQ88WF8Q8GG88B8", "8S888HGSB8FT8S8", "8MX88D88888T8K8", "8S8A88MGVDG8XK8", "M88S8B8I8M88J8N", "8W88X88ZT8KA8I8", "88SQGB8I8J88W88", "U88H8NI8CZB88B8", "8PK8H8T8888TQR8"};
+        String[] photo=new String[]{
+                "8D88888J8L8E888",
+                "88NKMG8N8E8JI88",
+                "888NS8EU88HN8EO",
+                "LUQ888A8TH8OIH8",
+                "888QJ88R8SG88TY",
+                "88ZQV88B88OUZ8O",
+                "FQ88WF8Q8GG88B8",
+                "8S888HGSB8FT8S8",
+                "8MX88D88888T8K8",
+                "8S8A88MGVDG8XK8",
+                "M88S8B8I8M88J8N",
+                "8W88X88ZT8KA8I8",
+                "88SQGB8I8J88W88",
+                "U88H8NI8CZB88B8",
+                "8PK8H8T8888TQR8"};
         Point[]test=new Point[19];
         test[0]=new Point(1,17);
         test[1]=new Point(3,3);
@@ -68,16 +99,39 @@ class icehockeyTest {
     @Test
     void test4() {
         icehockey x=new icehockey();
-        String[] photo=new String[]{"11111", "1AAA1", "1A1A1", "1AAA1", "11111" };
+        String[] photo=new String[]{
+                "11111",
+                "1AAA1",
+                "1A1A1",
+                "1AAA1",
+                "11111" };
         Point[]test=new Point[2];
         test[0]=new Point(5,5);
         test[1]=new Point(5,5);
         assertArrayEquals(test,x.findPlayers(photo,1,3));
     }
     @Test
-    void test_null_array_exception(){
+    void test5()
+    {
+        icehockey pr = new icehockey();
+        java.awt.Point [] actual= pr.findPlayers(new String []
+                {
+                        "111",
+                        "1X1",
+                        "111"
+                }, 1,32);
+        java.awt.Point [] expected = new java.awt.Point []
+                {
+                        new java.awt.Point(3,3),
+                };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void test_empty_array_handle(){
             icehockey x=new icehockey();
-            String[] photo=new String[2];//I make array of Strings its size is (2)and it is null
-            assertThrows(NullPointerException.class,()->{x.findPlayers(photo,5,9);});
+            String[] photo=new String[]{};//I make array of Strings its empty
+            Point[] p=new Point[]{};
+            assertArrayEquals(p,x.findPlayers(photo,1,3));
     }
 }
